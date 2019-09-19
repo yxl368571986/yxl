a8179f6c5c92ceff150c20a0dd4e635e57821de9
v_xinlyan(严星龙) <v_xinlyan@tencent.com>
2019-09-19 13:37:42 +0800

    第一次操作git

---
+++ set.js
@@ -0,0 +1,13 @@
+
+    // let set=new set();
+    // let arr=[1,2,3,4,5,6,1,2,5,3];
+    // arr.forEach(x=>set.add(x));
+    // console.log(set);
+
+    let set = new Set([1, 2,2,3,3, 4, 5]);
+    console.log([...set])
+
+
+    let arr = [1,2,3,3,5,5,4,4,6,6,8,8];
+    let arr2 = new Set(arr);
+    console.log([...arr2]);
